823c9a881c1b1614f88c89f01e86d164c517b9d7
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2019-12-15 01:12:41 +0900

    cursor animation

--- Assets/Main/Export/Scripts/Calibration.cs
+++ Assets/Main/Export/Scripts/Calibration.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Calibration : MonoBehaviour
 {
+
+    List<Vector3> m_UnityScreenPositions;
+
+    List<Vector3> m_AnalogInputPoints;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Main/Export/Scripts/CrosshairAnimation.cs
+++ Assets/Main/Export/Scripts/CrosshairAnimation.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class CrosshairAnimation : MonoBehaviour
 {
+    RectTransform m_Transform;
+
+    Vector3 m_InitialRotation = new Vector3(0f,0f,0f);
+    Vector3 m_InitialScale = new Vector3(1f, 1f, 1f);
+
+    Vector3 m_Scale = new Vector3(0.03f, 0.03f, 0f);
+
+    float m_RotationDelta = 0.7f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Transform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if ((transform.eulerAngles.z > 45f && transform.eulerAngles.z < 90f) || (transform.eulerAngles.z < 315f && transform.eulerAngles.z > 270f))
+        {
+            m_RotationDelta *= -1f;
+        }
+        transform.Rotate(0f, 0f, m_RotationDelta, Space.Self);
+
+        if (transform.localScale.x > 1.5f || transform.localScale.x < 1f)
+        {
+            m_Scale *= -1f;
+        }
+        transform.localScale += m_Scale;
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnEnable()
     {
         
     }
